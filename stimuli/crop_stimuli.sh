9c36265364ead4424de6956d0a544d62ee31ab1d
Geoff Brookshire <G.Brookshire@bham.ac.uk>
2019-10-25 14:04:35 +0100

    Change out one image

--- stimuli/crop_stimuli.sh
+++ stimuli/crop_stimuli.sh
@@ -4,7 +4,7 @@ convert raw/1.jpg -crop 420x420+100+2  cropped/1.jpg
 convert raw/2.jpg -crop 360x360+130+5  cropped/2.jpg
 convert raw/3.jpg -crop 440x440+20+20  cropped/3.jpg
 convert raw/4.jpg -crop 360x360+120+80 cropped/4.jpg
-convert raw/5.jpg -crop 460x460+100+5  cropped/5.jpg
+convert raw/5.jpg -crop 380x380+10+30  cropped/5.jpg
 
 for filename in cropped/*.jpg; do
     convert $filename -resize 256x256! $filename # Resize to a common size
