c67f1c98f57fbd58983ce55bbc26abf9f035c1a9
Geoff Brookshire <G.Brookshire@bham.ac.uk>
2019-10-25 13:55:21 +0100

    Normalize image histograms

--- stimuli/crop_stimuli.sh
+++ stimuli/crop_stimuli.sh
@@ -1,14 +1,14 @@
 # Crop images
-convert raw/0.jpg -crop 300x300+250+40 cropped/0.jpg
+convert raw/0.jpg -crop 250x250+275+60 cropped/0.jpg
 convert raw/1.jpg -crop 420x420+100+2  cropped/1.jpg
 convert raw/2.jpg -crop 360x360+130+5  cropped/2.jpg
 convert raw/3.jpg -crop 440x440+20+20  cropped/3.jpg
 convert raw/4.jpg -crop 360x360+120+80 cropped/4.jpg
-convert raw/5.jpg -crop 460x460+100+5 cropped/5.jpg
+convert raw/5.jpg -crop 460x460+100+5  cropped/5.jpg
 
-# Resize to a common size
 for filename in cropped/*.jpg; do
-    convert $filename -resize 256x256! $filename
+    convert $filename -resize 256x256! $filename # Resize to a common size
+    convert $filename -normalize "${filename}" # Normalize brightness histograms
 done
 
 # Print info to check that it worked
