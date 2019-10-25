44760e5c4cab3f8d98af2783abbcf88407858248
Geoff Brookshire <G.Brookshire@bham.ac.uk>
2019-10-25 12:32:49 +0100

    Cropped images more closely

--- stimuli/crop_stimuli.sh
+++ stimuli/crop_stimuli.sh
@@ -1,9 +1,9 @@
 # Crop images
-convert raw/0.jpg -crop 420x420+150+2 cropped/0.jpg
-convert raw/1.jpg -crop 420x420+100+2 cropped/1.jpg
-convert raw/2.jpg -crop 420x420+100+2 cropped/2.jpg
-convert raw/3.jpg -crop 440x440+20+20 cropped/3.jpg
-convert raw/4.jpg -crop 420x420+80+20 cropped/4.jpg
+convert raw/0.jpg -crop 300x300+250+40 cropped/0.jpg
+convert raw/1.jpg -crop 420x420+100+2  cropped/1.jpg
+convert raw/2.jpg -crop 360x360+130+5  cropped/2.jpg
+convert raw/3.jpg -crop 440x440+20+20  cropped/3.jpg
+convert raw/4.jpg -crop 360x360+120+80 cropped/4.jpg
 convert raw/5.jpg -crop 460x460+100+5 cropped/5.jpg
 
 # Resize to a common size
