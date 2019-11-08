b2c569e0b40586cad8a2cc0dbabc604a80e0e4ea
Geoff Brookshire <G.Brookshire@bham.ac.uk>
2019-11-08 15:32:35 +0000

    Load lattice of stimulus positions

---
+++ analysis/load_lattice.py
@@ -0,0 +1,17 @@
+""" Load up the positions of the stimuli
+"""
+
+import json
+import sys
+sys.path.append('../exp-scripts')
+from lattice import lattice
+import dist_convert as dc 
+
+def stimulus_positions():
+    exp_info = json.load(open('exp_info.json')) 
+    win_center = (0, 0) 
+    stim_dist = int(dc.deg2pix(exp_info['stim_dist_deg')) # Min distance between images
+    stim_locs = lattice(center=win_center, n_fold=6, radius=stim_dist, n_layers=2)
+    stim_locs = [dc.origin_psychopy2eyelink(pos) for pos in stim_locs]
+    return stim_locs
+
