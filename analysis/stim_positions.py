9687559580001917d59367fcce9643772a4a879e
Geoff Brookshire <G.Brookshire@bham.ac.uk>
2019-11-08 15:34:34 +0000

    Get the stimulus positions

--- analysis/stim_positions.py
+++ analysis/stim_positions.py
@@ -7,10 +7,10 @@ sys.path.append('../exp-scripts')
 from lattice import lattice
 import dist_convert as dc 
 
-def stimulus_positions():
-    exp_info = json.load(open('exp_info.json')) 
+def stim_positions():
+    expt_info = json.load(open('expt_info.json')) 
     win_center = (0, 0) 
-    stim_dist = int(dc.deg2pix(exp_info['stim_dist_deg')) # Min distance between images
+    stim_dist = int(dc.deg2pix(expt_info['stim_dist_deg'])) # Min distance between images
     stim_locs = lattice(center=win_center, n_fold=6, radius=stim_dist, n_layers=2)
     stim_locs = [dc.origin_psychopy2eyelink(pos) for pos in stim_locs]
     return stim_locs
