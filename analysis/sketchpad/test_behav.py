bdb666f5f4f27d7185c08b00da810becb081d273
Geoff Brookshire <G.Brookshire@bham.ac.uk>
2019-11-21 10:28:52 +0000

    Clean up plots and filename specification

--- analysis/sketchpad/test_behav.py
+++ analysis/sketchpad/test_behav.py
@@ -1,13 +1,22 @@
+"""
+Plot behavioral results for one subject
+"""
+
+import json
 import pandas as pd
 import matplotlib.pyplot as plt
 
-fnames = {'meg': '191104/yalitest.fif',
-          'eye': '19110415.asc',
-          'behav': '2019-11-04-1527.csv'}
+# Which subject to run
+n = 1
 
-data_dir = '../data/'
+expt_info = json.load(open('expt_info.json'))
+subject_info = pd.read_csv(expt_info['data_dir'] + 'subject_info.csv')
 
-behav = pd.read_csv(data_dir + 'logfiles/' + fnames['behav'])
+data_dir = expt_info['data_dir']
+
+# Read in the behavioral logfile
+behav_fname = f'{data_dir}logfiles/{subject_info["behav"][n]}.csv' 
+behav = pd.read_csv(behav_fname)
 
 # Only keep trials that were actually run
 behav = behav.loc[behav['ran'] == 1.0]
@@ -24,11 +33,13 @@ behav['correct'] = behav['mem_target_loc'] == behav['resp_side']
 f, (a0, a1) = plt.subplots(1, 2, gridspec_kw={'width_ratios': [1, 3]})
 
 # Plot accuracy
-a0.bar(0.5, behav['correct'].mean())
-a0.set_xlim([-1, 2])
+acc = behav['correct'].mean()
+a0.bar(0.5, acc)
+a0.set_xlim([-0.5, 1.5])
 a0.set_xlabel('')
 a0.set_xticks([])
 a0.set_ylabel('Accuracy')
+a0.text(0.5, 0.2, f'{acc*100:.1f}%', horizontalalignment='center')
 
 # Plot histogram of RTs
 a1.hist(behav['rt'])
@@ -36,5 +47,6 @@ a1.set_xlim([0, 4])
 a1.set_xlabel('Time (s)')
 a1.set_ylabel('Count')
 
+f.set_size_inches(6, 2)
 f.tight_layout()
 plt.show()
