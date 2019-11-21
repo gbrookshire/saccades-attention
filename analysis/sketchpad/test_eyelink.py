22facfdbe1641a6a5b7aef063d8f28f4f4ba0eae
Geoff Brookshire <G.Brookshire@bham.ac.uk>
2019-11-21 15:50:55 +0000

    Histograms of saccade and fixation duration

--- analysis/sketchpad/test_eyelink.py
+++ analysis/sketchpad/test_eyelink.py
@@ -3,6 +3,7 @@
 
 import matplotlib.pyplot as plt
 import eyelink_parser
+import load_data
 
 # Get the positions of the stimuli
 import sys
@@ -17,19 +18,47 @@ win_center = (0, 0)
 stim_locs = lattice(center=win_center, n_fold=6, radius=STIM_DIST, n_layers=2)
 stim_locs = [dc.origin_psychopy2eyelink(pos) for pos in stim_locs]
 
-fname = '../data/eyelink/ascii/19110415.asc'
-edata = eyelink_parser.EyelinkData(fname)
 
-# Only keep fixations to the screen
-fix = edata.fixations
-fix = fix[(fix['x_avg'] > 0) & (fix['y_avg'] > 0)]
+n = 1
+d = load_data.load_data(n)
+fix = d['fix_info']
 
-# Plot fixations
+# Set up the grid of subplots
+f = plt.figure(constrained_layout=True,
+               figsize=(6, 3))
+gs0 = f.add_gridspec(1, 2)
+gs_left = gs0[0].subgridspec(1, 1)
+gs_right = gs0[1].subgridspec(2, 1)
+
+# Plot fixation positions
+f.add_subplot(gs_left[0,0])
 plt.plot(fix['x_avg'], fix['y_avg'],
-         'o', fillstyle='none', alpha=0.5)
-
+         'o', fillstyle='none', alpha=0.5) 
 # Plot lattice locations
 x,y = list(zip(*stim_locs))
 plt.plot(x, y, '*r')
+ax = plt.gca()
+ax.axes.get_xaxis().set_visible(False)
+ax.axes.get_yaxis().set_visible(False)
+
+# Histogram of fixation duration
+f.add_subplot(gs_right[0,0])
+plt.hist(fix['dur'] / 1000,
+         bins=25, density=True)
+plt.xlabel('Fixation Duration (s)')
+plt.ylabel('Density')
+
+# Plot histogram of inter-saccade interval
+f.add_subplot(gs_right[1,0])
+onsets = fix['start'][1:].to_numpy()
+offsets = fix['end'][:-1].to_numpy()
+isi = onsets - offsets
+isi = isi / 1000
+isi = isi[isi < 0.15]
+plt.hist(isi,
+         bins=25, density=True) 
+plt.xlabel('Saccade Duration (s)')
+plt.ylabel('Density')
+plt.xlim(0, 0.150)
 
 plt.show()
