7b6beefce00286ad76f8cf1eac6a596dd5beecc0
gbrookshire <g.brookshire@bham.ac.uk>
2019-12-02 12:07:11 +0000

    Set up for analyzing multiple subjects

--- analysis/sketchpad/test_behav.py
+++ analysis/sketchpad/test_behav.py
@@ -3,50 +3,84 @@ Plot behavioral results for one subject
 """
 
 import json
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Which subject to run
-n = 1
-
 expt_info = json.load(open('expt_info.json'))
-subject_info = pd.read_csv(expt_info['data_dir'] + 'subject_info.csv')
-
+subject_info = pd.read_csv(expt_info['data_dir'] + 'subject_info.csv') 
 data_dir = expt_info['data_dir']
 
-# Read in the behavioral logfile
-behav_fname = f'{data_dir}logfiles/{subject_info["behav"][n]}.csv' 
-behav = pd.read_csv(behav_fname)
+def analyze_behavior(n):
+    """ Analyze data for one subject 
+    Return accuracy and RT
+    """ 
+    # Read in the behavioral logfile
+    behav_fname = f'{data_dir}logfiles/{subject_info["behav"][n]}.csv' 
+    behav = pd.read_csv(behav_fname) 
+    # Only keep trials that were actually run
+    behav = behav.loc[behav['ran'] == 1.0] 
+    # Get response accuracy
+    behav['resp_side'] = ''
+    behav.loc[behav['resp'] == '4', 'resp_side'] = 'left'
+    behav.loc[behav['resp'] == '7', 'resp_side'] = 'right'
+    behav['correct'] = behav['mem_target_loc'] == behav['resp_side'] 
+    # Get variables to return
+    acc = behav['correct'].mean()
+    rt = behav['rt'].astype(float) 
+    return acc, rt
 
-# Only keep trials that were actually run
-behav = behav.loc[behav['ran'] == 1.0]
 
-behav['rt'] = behav['rt'].astype(float)
+def analyze_all_subjects():
+    results = []
+    for n in subject_info.index:
+        if subject_info['exclude'][n]:
+            continue
+        else:
+            results.append(analyze_behavior(n))
 
-# Get response accuracy
-behav['resp_side'] = ''
-behav.loc[behav['resp'] == '4', 'resp_side'] = 'left'
-behav.loc[behav['resp'] == '7', 'resp_side'] = 'right'
-behav['correct'] = behav['mem_target_loc'] == behav['resp_side']
+    # Get lists of accuracy and RT distributions for each subject
+    acc, rt = zip(*results)
 
-# Set up plot layout
-f, (a0, a1) = plt.subplots(1, 2, gridspec_kw={'width_ratios': [1, 3]})
+    # Plot the results
+    # Set up plot layout
+    f, (a0, a1) = plt.subplots(1, 2, gridspec_kw={'width_ratios': [1, 3]})
 
-# Plot accuracy
-acc = behav['correct'].mean()
-a0.bar(0.5, acc)
-a0.set_xlim([-0.5, 1.5])
-a0.set_xlabel('')
-a0.set_xticks([])
-a0.set_ylabel('Accuracy')
-a0.text(0.5, 0.2, f'{acc*100:.1f}%', horizontalalignment='center')
+    # Plot accuracy
+    a0.bar(0.5, np.mean(acc),
+           color='none', edgecolor='blue') 
+    a0.plot(0.5 + np.zeros(len(acc)), acc, 'o',
+            fillstyle='none', color='blue')
+    a0.text(0.5, 0.2,
+            f'{np.mean(acc)*100:.1f}%',
+            horizontalalignment='center')
+    a0.set_xlim([-0.5, 1.5])
+    a0.set_xticks([])
+    a0.set_xlabel('')
+    a0.set_ylim(0, 1.0)
+    a0.set_yticks([0, 0.5, 1.0])
+    a0.set_ylabel('Accuracy')
 
-# Plot histogram of RTs
-a1.hist(behav['rt'])
-a1.set_xlim([0, 4])
-a1.set_xlabel('Time (s)')
-a1.set_ylabel('Count')
+    # Plot RT histograms for each subject
+    for subj_rt in rt:
+        a1.hist(subj_rt,
+                bins=20,
+                histtype='step',
+                color='blue')
+    a1.set_xlim([0, 4])
+    a1.set_xlabel('Time (s)')
+    a1.set_ylabel('Count')
 
-f.set_size_inches(6, 2)
-f.tight_layout()
-plt.show()
+    # Format the plot
+    f.set_size_inches(6, 2)
+    f.tight_layout()
+    plt.show()
+
+
+if __name__ == '__main__':
+    analyze_all_subjects()
+
+
+
+
+
