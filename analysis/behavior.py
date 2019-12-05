1d8e21a40dac2ea3f4c0338282ff0106f685759e
Geoff Brookshire <G.Brookshire@bham.ac.uk>
2019-12-05 10:59:12 +0000

    Behavioral analysis working with full test subjects

--- analysis/behavior.py
+++ analysis/behavior.py
@@ -14,23 +14,32 @@ expt_info = json.load(open('expt_info.json'))
 subject_info = pd.read_csv(expt_info['data_dir'] + 'subject_info.csv') 
 data_dir = expt_info['data_dir']
 
+def coerce_numeric(x):
+    """ Coerce a vector of strings into numeric data
+    """
+    if x.dtype == np.number:
+        return x.to_numpy()
+    else:
+        return np.genfromtxt(x)
+
 def analyze_behavior(n):
     """ Analyze data for one subject 
     Return accuracy and RT
     """ 
     # Read in the behavioral logfile
     behav_fname = f'{data_dir}logfiles/{subject_info["behav"][n]}.csv' 
-    behav = pd.read_csv(behav_fname) 
+    col_dtypes = {'resp': str}
+    behav = pd.read_csv(behav_fname, dtype=col_dtypes)
     # Only keep trials that were actually run
     behav = behav.loc[behav['ran'] == 1.0] 
     # Get response accuracy
     behav['resp_side'] = ''
-    behav.loc[behav['resp'] == '4', 'resp_side'] = 'left'
-    behav.loc[behav['resp'] == '7', 'resp_side'] = 'right'
+    behav.loc[behav['resp'] == '7', 'resp_side'] = 'left'
+    behav.loc[behav['resp'] == '8', 'resp_side'] = 'right'
     behav['correct'] = behav['mem_target_loc'] == behav['resp_side'] 
     # Get variables to return
-    acc = behav['correct'].mean()
-    rt = behav['rt'].astype(float) 
+    acc = behav['correct'].mean() 
+    rt = coerce_numeric(behav['rt'])
     return acc, rt
 
 
@@ -44,8 +53,10 @@ def analyze_all_subjects():
 
     # Get lists of accuracy and RT distributions for each subject
     acc, rt = zip(*results)
+    plot_results(acc, rt)
 
-    # Plot the results
+
+def plot_results(acc, rt):
     # Set up plot layout
     f, (a0, a1) = plt.subplots(1, 2, gridspec_kw={'width_ratios': [1, 3]})
 
@@ -66,6 +77,7 @@ def analyze_all_subjects():
 
     # Plot RT histograms for each subject
     for subj_rt in rt:
+        subj_rt = subj_rt[~np.isnan(subj_rt)]
         a1.hist(subj_rt,
                 bins=20,
                 histtype='step',
