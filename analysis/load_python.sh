8ae5656f6bb552e36b2ca7187ed179f96d4d33e0
Geoff Brookshire <G.Brookshire@bham.ac.uk>
2020-01-17 18:40:39 +0000

    Specify sklearn 0.21.3

--- analysis/load_python.sh
+++ analysis/load_python.sh
@@ -2,3 +2,4 @@ module load bear-apps/2019a
 module load Python/3.7.2-GCCcore-8.2.0 
 module load IPython/7.7.0-foss-2019a-Python-3.7.2
 module load MNE-Python/0.19.1-foss-2019a-Python-3.7.2
+module switch scikit-learn/0.21.3-foss-2019a-Python-3.7.2
