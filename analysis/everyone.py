445c1b0b9952bb7a8c93fc2e526cd30e62dd0e49
Geoff Brookshire <G.Brookshire@bham.ac.uk>
2020-02-05 11:50:30 +0000

    Send analyses to sbatch for each subject

--- analysis/everyone.py
+++ analysis/everyone.py
@@ -1,21 +1,51 @@
+#!/usr/bin/env python3
 """
 Do something for each subject
 """
 
+import os
+import sys
 import json
-import pandas as pd
+import csv
 
 expt_info = json.load(open('expt_info.json'))
-subject_info = pd.read_csv(expt_info['data_dir'] + 'subject_info.csv',
-                           engine='python', sep=',')
+with open(expt_info['data_dir'] + 'subject_info.csv', 'r') as f:
+    reader = csv.DictReader(f)
+    subject_info = list(reader)
 
-def apply(fnc):
+def apply_fnc(fnc):
     """ Apply the function to each non-excluded subject.
+
+    fnc: A function that takes a row of `subject_info.csv`
     """
     results = []
-    for inx, row in subject_info.iterrows():
-        if not row['exclude']:
+    for row in subject_info:
+        if row['exclude'] != '1':
             res = fnc(row)
             results.append(res)
     return results
 
+
+def slurm(cmd):
+    """
+    Pass arguments on to sbatch for all subjects
+
+    Args:
+    cmd: A command to pass to `slurmit`. Write `<N>` for subject number (str)
+
+    Example:
+    > python3 everyone.py "'rsa_item.py <N>' -t 15:00 -m 30G -c 5"
+    Make sure to use double quotes around the whole slurm string, and single
+    quotes around the part that goes to the python call.
+    """
+    cmd_template = './slurmit ' + cmd
+    number_tag = '<N>'
+    def slurm_call(row):
+        c = cmd_template.replace(number_tag, str(row['n']))
+        print(c)
+        os.system(c)
+    results = apply_fnc(slurm_call)
+
+
+if __name__ == '__main__':
+    slurm(sys.argv[1])
